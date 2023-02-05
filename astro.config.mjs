f18803104cf2f46a98875ecec99f31e004d1ecff
arch-spatula <shkim516@naver.com>
2023-02-05 23:33:00 +0900

    Fix, 포스팅 업데이트 확인 커밋 #1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://arch-spatula.github.io",
+  markdown: {
+    drafts: true,
+  },
+  integrations: [
+    mdx({
+      drafts: true,
+    }),
+  ],
 });
