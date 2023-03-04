20ac4192571ebf78f3ecbaa1612861bbbba797ec
arch-spatula <shkim516@naver.com>
2023-03-05 04:14:56 +0900

    feat: code block button #8
    - lit 설치
    - code block theme 반영
    - code block font 반영
    - code block copy button 추가
    - code block copy button hover에만 보이도록 조정
    - 문단 양끝 정렬

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ export default defineConfig({
   site: "https://arch-spatula.github.io",
   markdown: {
     drafts: true,
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "poimandres",
+      wrap: true,
+    },
   },
 });
---
+++ src/components/CopyCodeButton/create-copy-buttons.ts
@@ -0,0 +1,8 @@
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+  let button = document.createElement("copy-code-button");
+  block.appendChild(button);
+});
+
+export {};
