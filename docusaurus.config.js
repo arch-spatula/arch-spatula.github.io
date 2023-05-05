4d6760786c8472408792795e157051ef892c56f7
arch-spatula <shkim516@naver.com>
2023-05-05 19:55:43 +0900

    chore: set orgName and projectName #17

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,13 @@ const config = {
   url: "https://arch-spatula.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/arch-spatula.github.io/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "arch-spatula", // Usually your GitHub org/user name.
+  projectName: "arch-spatula.github.io", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
