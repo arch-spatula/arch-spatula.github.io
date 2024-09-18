ed14a5c24090c3b183ab6b707af3a7d9a3dede6d
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-09-18 14:03:26 +0900

    feat: nav 기능 추가 #338
    feat: nav 기능 추가 #338

--- app.vue
+++ app.vue
@@ -1,5 +1,66 @@
+<script setup lang="ts">
+useSeoMeta({
+  ogImage: "/og-image.png",
+});
+useHead({
+  link: [{ rel: "icon", type: "image/svg+xml", href: "favicon.svg" }],
+});
+</script>
 <template>
+  <header class="header">
+    <nav class="nav">
+      <ul class="nav-list">
+        <li class="nav-item" style="height: 44px">
+          <NuxtLink class="profile" to="/">
+            <NuxtImg src="/profile.png" height="44" />
+          </NuxtLink>
+        </li>
+        <li class="nav-item">
+          <NuxtLink
+            to="https://github.com/arch-spatula/arch-spatula.github.io"
+            target="_blank"
+          >
+            <p class="github">GitHub</p>
+            <!--
+             TODO: 새탭 아이콘 추가하기
+				    -->
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
   <div>
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  background-color: #020420;
+  height: 64px;
+  .nav {
+    /* max-width: 800px; */
+    height: 100%;
+    box-sizing: border-box;
+    /* margin: 0px auto; */
+    margin: 0px 12px;
+    display: flex;
+    place-items: center;
+    .nav-list {
+      display: flex;
+      flex: 1;
+      align-items: center;
+      justify-content: space-between;
+
+      .nav-item {
+        height: fit-content;
+
+        .github {
+          color: #fff;
+          font-size: 20px;
+        }
+      }
+    }
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/content'],
-  routeRules: {
-    '/': { prerender: true }
-  }
-})
+	devtools: { enabled: true },
+	modules: ["@nuxt/content", "@nuxt/image"],
+	css: ["~/assets/css/main.css"],
+
+	routeRules: {
+		"/": { prerender: true },
+	},
+
+	compatibilityDate: "2024-09-18",
+});
