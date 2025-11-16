484f1cea2f11ed53419859eeb42abaf20632c655
김상현 <84452145+arch-spatula@users.noreply.github.com>
2025-11-16 22:44:22 +0900

    wip: 읽기 로직 구현 중 #346

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 - node.js로 빌드를 처리.
 - typescript로 로직을 처리.
 - pnpm으로 패키지 매니징.
+
+## 프로젝트 시작하기
+
+```sh
+git clone https://github.com/arch-spatula/arch-spatula.github.io.git
+```
+
+```sh
+pnpm run i # 패키지 설치
+```
+
---
+++ app/server.ts
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview 서버를 실행하는 루트 파일
+ * 
+ * @todo vite의 서버처럼 파일 변경이 있으면 변경이 있는 파일만 다시 빌드하고 서버를 리로드하는 기능
+ */
+
+/**
+ * 서버를 실행하는 함수
+ */
+const serve = () => {
+    console.log('Server is running on port 3000');
+}
+
+serve();
